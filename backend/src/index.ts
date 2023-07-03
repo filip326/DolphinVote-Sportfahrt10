75245110a6596c2b33e02c2e6d7dbbf683ebb918
Christian F <109520303+Barsch2006@users.noreply.github.com>
2023-07-03 21:26:20 +0000

    update backend

---
+++ backend/src/index.ts
@@ -0,0 +1,47 @@
+import { config as dotenv } from "dotenv";
+dotenv();
+
+import express from "express";
+import cookieParser from "cookie-parser";
+import { MongoClient, WithId } from "mongodb";
+const app = express();
+
+import IUser from './types/user'
+import { join } from "path";
+
+declare global {
+  namespace Express {
+    interface User extends WithId<IUser> { }
+  }
+}
+
+import auth from "./auth";
+
+async function main() {
+
+  const client = new MongoClient(process.env.MONGO_URL ?? "");
+  await client.connect();
+  const db = client.db(process.env.DB_NAME ?? "ATec");
+
+  app.use(express.json());
+  app.use(cookieParser());
+
+  app.use(auth(db));
+
+  app.use(express.static(process.env.PUBLIC_DIR ?? "public"));
+
+  app.use((req, res) => {
+    if (req.method === "GET") {
+      res.sendFile(join(process.env.PUBLIC_DIR ?? "public", "index.html"));
+      return;
+    } else {
+      return;
+    }
+  })
+
+  app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+  });
+}
+
+main();
