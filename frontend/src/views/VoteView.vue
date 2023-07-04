b7522b02b9de67f09f9189560a22fc723a6883b0
Christian F <109520303+Barsch2006@users.noreply.github.com>
2023-07-04 17:12:29 +0000

    small changes

--- frontend/src/views/VoteView.vue
+++ frontend/src/views/VoteView.vue
@@ -1,121 +1,25 @@
 <script lang="ts">
+// import IVoting from './types/vote'
 
 export default {
     data() {
         return {
-            openVotings: [
-                // sample
-                {
-                    voting: 'Voting 1',
-                    open: true
-                },
-                {
-                    voting: 'Voting 2',
-                    open: false
-                }
-            ] as { voting: string, open: boolean, _id: string }[],
-            votingResults: {
-                "Sample": "Activity 1"
-            } as {
-                [title: string]: string
-            }
-        }
-    },
-    methods: {
-        async loadOpenVotings() {
-            // http get /votings
-            // reponse: [ { voting: string, open: boolean }]
-
-            const response = await fetch('/votings', {
-                method: 'GET',
-            });
-
-            if (response.status === 200) {
-                const data = await response.json() as { voting: string, open: boolean, _id: string }[];
-                this.openVotings = data;
-            } else throw new Error("Error loading open votings");
-
-        },
-        async loadVotingResults() {
-            // http get /results
-            // reponse: { [title: string]: string }
-
-            const response = await fetch('/results', {
-                method: 'GET',
-            });
-
-            if (response.status === 200) {
-                const data = await response.json() as { [title: string]: string };
-                this.votingResults = data;
-            } else throw new Error("Error loading voting results");
-        }
-    },
-    async beforeMount() {
-        try {
-            await Promise.all([this.loadOpenVotings(), this.loadVotingResults()]);
-        } catch {
-            // this.$router.push("/")
+            votings: Array<any>(),
         }
     }
 }
-
 </script>
 
 <template>
-    <v-card>
-        <v-card-title>
-            Wahlen
-        </v-card-title>
-        <v-card-subtitle>
-            Hier kannst du wählen
-        </v-card-subtitle>
-        <v-card-text>
-            <v-list>
-                <v-list-item v-for="(vote, index) in openVotings" :key="index">
-                    <v-list-item-title>{{ vote.voting }}</v-list-item-title>
-                    <v-list-item-action>
-                        <v-btn v-if="vote.open" variant="outlined">Wählen</v-btn>
-                        <div v-else class="vote-closed">Bereits gewählt</div>
-                    </v-list-item-action>
-                </v-list-item>
-            </v-list>
-        </v-card-text>
-    </v-card>
-    <v-card>
-        <v-card-title>
-            Ergebnisse
-        </v-card-title>
-        <v-card-subtitle>
-            Du wurdest zu folgenden Aktivitäten zugeteilt
-        </v-card-subtitle>
-        <v-card-text>
-            <v-list v-if="Object.keys(votingResults).length > 0">
-                <v-list-item v-for="result in Object.keys(votingResults)" :key="result">
-                    <v-list-item-title>{{ result }}</v-list-item-title>
-                    <div>
-                        {{ votingResults[result] }}
-                    </div>
-                </v-list-item>
-            </v-list>
-            <p v-else>
-                Es wurden noch keine Ergebnisse bekanntgegeben.
-            </p>
-        </v-card-text>
-    </v-card>
+    <v-list>
+        <v-list-item>
+            <v-list-item-title v-for="voting, index in votings">
+                {{ voting.name }}
+            </v-list-item-title>
+        </v-list-item>
+    </v-list>
 </template>
 
-<style>
-.v-list-item {
-    margin: 5px 0;
-    padding: 10px 5px;
-    border: 1px solid rgba(255, 255, 255, 0.559);
-    border-radius: 10px;
-}
-</style>
-
 <style scoped>
-.vote-closed {
-    color: red;
-}
 </style>
 ```
